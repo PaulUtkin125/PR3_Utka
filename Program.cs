219cb86baf71cbb43161a3fceac87c7eebed1d83
student <ttt@mail.ru>
2024-04-19 15:43:59 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR3_Utkin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ввести А - 0; ввести В - 1; выполнить умножение - 2; выполнить деление - 3; выполнить сложение - 4; выполнить вычитание - 5");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
